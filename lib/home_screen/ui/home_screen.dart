0c3f0d393f42f51580091412308894a3c4f57e09
Qurt <leilasal2007@gmail.com>
2026-02-02 15:48:51 +0500

    add homescreen

--- lib/home_screen/ui/home_screen.dart
+++ lib/home_screen/ui/home_screen.dart
@@ -13,22 +13,36 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.background,
-      body: Padding(padding:const EdgeInsets.symmetric(horizontal: 27.0),
-      child: Column(
-      children: [
-        Container(
-          width: double.infinity,
-          padding: EdgeInsets.all(30),
-          decoration: BoxDecoration(
-          color: AppColors.container,
-          borderRadius: BorderRadius.circular(20),
-          ),
-          child: Text(
-            'placeholder'
-          ),
-        )
-      ],
-      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 27.0),
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                color: AppColors.container,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Calories counter',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 24, color: AppColors.subText),
+                  ),
+                  Text('Overall: 6767 cal',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 40,
+                    color: AppColors.mainText
+                  ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/list_item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ListItem extends StatelessWidget {
+  final String product;
+  final int calorieCount;
+
+
+
+  const ListItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
